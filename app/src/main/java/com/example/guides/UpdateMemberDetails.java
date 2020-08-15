4d135c725f38694cbe2246da29186d2cc4a5066e
IT19180380 <68540529+IT19180380@users.noreply.github.com>
2020-08-15 23:13:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/guides/UpdateMemberDetails.java
@@ -0,0 +1,13 @@
+package com.example.guides;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateMemberDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_member_details);
+    }
+}
