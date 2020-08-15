879d814e9ce165be2f5fc66867e9f378759c3d1c
IT19180380 <68540529+IT19180380@users.noreply.github.com>
2020-08-16 01:22:14 +0530

    fourth update
    implemet interfaces and String reesources

---
+++ app/src/main/java/com/example/guides/GuideWitoutVehicleVeiw.java
@@ -0,0 +1,13 @@
+package com.example.guides;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuideWitoutVehicleVeiw extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_witout_vehicle_veiw);
+    }
+}
