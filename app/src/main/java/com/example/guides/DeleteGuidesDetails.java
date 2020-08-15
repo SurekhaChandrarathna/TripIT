4fca8b974a5ae028c4725627c5e7da56ca90de57
IT19180380 <68540529+IT19180380@users.noreply.github.com>
2020-08-16 00:13:11 +0530

    secound update
    secound update

---
+++ app/src/main/java/com/example/guides/DeleteGuidesDetails.java
@@ -0,0 +1,13 @@
+package com.example.guides;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeleteGuidesDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_guides_details);
+    }
+}
