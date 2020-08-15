b02bc1e257bb03b2658045616aad603b6b1063df
Sandamali <lakshika.sthennakoon@gmail.com>
2020-08-15 20:43:04 +0530

    first UI

---
+++ app/src/main/java/com/example/shops/shops_UI2_cat.java
@@ -0,0 +1,14 @@
+package com.example.shops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shops_UI2_cat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shops__u_i2_cat);
+    }
+}
