f1709ab4200d25d0858036bcc657b9f957700965
Sandamali <lakshika.sthennakoon@gmail.com>
2020-08-16 14:29:32 +0530

    NExt3 Update...

---
+++ app/src/main/java/com/example/shops/Shop_UI8.java
@@ -0,0 +1,14 @@
+package com.example.shops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shop_UI8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop__u_i8);
+    }
+}
