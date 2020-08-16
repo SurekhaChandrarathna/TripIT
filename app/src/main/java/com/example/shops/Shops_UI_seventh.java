2205ae6f3419c2b313c1d83aaef8ca362c9053bf
Sandamali <lakshika.sthennakoon@gmail.com>
2020-08-16 13:11:26 +0530

    Next Update....

---
+++ app/src/main/java/com/example/shops/Shops_UI_seventh.java
@@ -0,0 +1,14 @@
+package com.example.shops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shops_UI_seventh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shops__u_i_seventh);
+    }
+}
