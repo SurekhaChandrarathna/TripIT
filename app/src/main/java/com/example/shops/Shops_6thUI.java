43d96cee2550b16d3bc8d4aeaaff88fcfc6bc039
Sandamali <lakshika.sthennakoon@gmail.com>
2020-08-16 10:43:41 +0530

    6th UI

---
+++ app/src/main/java/com/example/shops/Shops_6thUI.java
@@ -0,0 +1,14 @@
+package com.example.shops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shops_6thUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shops_6th_u_i);
+    }
+}
