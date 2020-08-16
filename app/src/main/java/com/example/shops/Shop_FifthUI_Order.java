75a4c03f9c562bacd98cae5be1f2365b4b27a92c
Sandamali <lakshika.sthennakoon@gmail.com>
2020-08-16 08:41:43 +0530

    Third Ui and 4th

---
+++ app/src/main/java/com/example/shops/Shop_FifthUI_Order.java
@@ -0,0 +1,14 @@
+package com.example.shops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shop_FifthUI_Order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop__fifth_u_i__order);
+    }
+}
---
+++ app/src/main/java/com/example/shops/Shops_UI_3.java
@@ -0,0 +1,14 @@
+package com.example.shops;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shops_UI_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shops__u_i_3);
+    }
+}
